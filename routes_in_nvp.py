f082b2bcbb96611faf8809cf9b85224ec4a63be3
VioletEverg <belabelllil@gmail.com>
2024-06-07 14:28:33 +0300

    update routes_in_nvp

--- routes_in_nvp.py
+++ routes_in_nvp.py
@@ -4,6 +4,7 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
 
 from save_prot import save_prot
 
@@ -11,10 +12,10 @@ from save_prot import save_prot
 def nvp_routes(driver, login, password, server, operation_type, start_date, end_date, out_dir):
     if server == "Москва":
         login_url = 'http://179'
-        table_url = 'http://179/index.html'
+        table_url = 'http://179/Report.jsp'
     else:
         login_url = 'http://206'
-        table_url = 'http://206/index.html'
+        table_url = 'http://206/Report.jsp'
 
     # Запуск НВП в Firefox
     driver.get(login_url)
@@ -32,18 +33,11 @@ def nvp_routes(driver, login, password, server, operation_type, start_date, end_
     # Переход на журнал перерасчетов
     driver.get(table_url)
 
-    driver.switch_to.frame("navigation_tree")
-    mass_op = driver.find_element(By.ID, 'form1:actionbarIndBez_link')
-    mass_op.click()
-    log = driver.find_element(By.ID, 'form1:text77')
-    log.click()
-    driver.switch_to.parent_frame()
-
-    # Работа внутри журнала
-    driver.switch_to.frame("detail")
-
     # Ждем загрузки журнала
     WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, 'form1:table1')))
+
+    select = Select(driver.find_element(By.ID, "form1:menu3"))
+    select.select_by_value("2")
 
     ind = 0
     last_row_in_page = 29
